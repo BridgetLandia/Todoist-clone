eef65ac7dca801aa5aac331a68d609a5dca67a7b
Brigitta Tóth <52370131+BridgetLandia@users.noreply.github.com>
2020-05-05 11:35:06 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-	apiKey: 'AIzaSyDuOIZzdm16_kbV4t067UYwZdnZiOMSyVc',
-	authDomain: 'todoist-737e9.firebaseapp.com',
-	databaseURL: 'https://todoist-737e9.firebaseio.com',
-	projectId: 'todoist-737e9',
-	storageBucket: 'todoist-737e9.appspot.com',
-	messagingSenderId: '621906808444',
-	appId: '1:621906808444:web:44f13feb0cb8102bd403f9'
+	apiKey: '',
+	authDomain: '',
+	databaseURL: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: ''
 });
 
 export { firebaseConfig as firebase };
