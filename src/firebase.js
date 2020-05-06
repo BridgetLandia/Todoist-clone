af94db5dd2d94b443bb9bd48b6d9129e435127b0
Brigitta <brigitta.tth@gmail.com>
2020-05-06 13:36:44 +0200

    Basic features

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-const firebaseConfig = firebase.initializeApp({
-	apiKey: 'AIzaSyDuOIZzdm16_kbV4t067UYwZdnZiOMSyVc',
-	authDomain: 'todoist-737e9.firebaseapp.com',
-	databaseURL: 'https://todoist-737e9.firebaseio.com',
-	projectId: 'todoist-737e9',
-	storageBucket: 'todoist-737e9.appspot.com',
-	messagingSenderId: '621906808444',
-	appId: '1:621906808444:web:44f13feb0cb8102bd403f9'
-});
+const firebaseConfig = firebase.initializeApp({});
 
 export { firebaseConfig as firebase };
