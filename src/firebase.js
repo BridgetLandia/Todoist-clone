cb01eafecb290fd48a78a2c1569a9f3e12d9b82f
Brigitta <brigitta.tth@gmail.com>
2020-05-06 13:45:28 +0200

    Basics

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-<<<<<<< HEAD
-const firebaseConfig = firebase.initializeApp({});
-=======
 const firebaseConfig = firebase.initializeApp({
 	apiKey: '',
 	authDomain: '',
@@ -13,6 +10,5 @@ const firebaseConfig = firebase.initializeApp({
 	messagingSenderId: '',
 	appId: ''
 });
->>>>>>> eef65ac7dca801aa5aac331a68d609a5dca67a7b
 
 export { firebaseConfig as firebase };
